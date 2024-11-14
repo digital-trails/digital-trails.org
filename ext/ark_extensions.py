ec16c871fac8cc64ddd88788a2e0d4878e6a8a11
Mark Rucker <rucker.mark@gmail.com>
2024-11-14 08:17:06 -0500

    Cleaned up Ark extensions.

--- ext/ark_extensions.py
+++ ext/ark_extensions.py
@@ -1,9 +1,9 @@
 import ark
-import markdown2
 
 @ark.renderers.register('md')
 def render_markdown(text):
     #the two extras below are here to support syntax highlighting
     #code-friendly: https://github.com/trentm/python-markdown2/wiki/code-friendly
     #fenced-code-blocks: https://github.com/trentm/python-markdown2/wiki/fenced-code-blocks
+    import markdown2
     return markdown2.markdown(text,extras=["code-friendly","fenced-code-blocks"])
